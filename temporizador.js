7f45f02b6b58b2b5a3b7ebda758e023065856ad8
bmagalhaes-coder <bmagalhaes@acad.ifma.edu.br>
2026-03-09 00:59:47 -0300

    Update temporizador.js

--- temporizador.js
+++ temporizador.js
@@ -3,12 +3,12 @@
     const logo = document.querySelector('.logo');
     logo.style.transition = 'opacity 1s ease'; // duração 1 segundo
     logo.style.opacity = '0';
-    }, 3700);
+    }, 2700);
 
   //Após 5 segundos, redireciona para mapa1.html
   setTimeout(() => {
     window.location.href = "mapa1.html";
-    }, 5000);
+    }, 4000);
 
   // Redireciona imediatamente se clicar na tela
   document.body.addEventListener('click', () => {

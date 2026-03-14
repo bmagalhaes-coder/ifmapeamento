ca62e37e6e62563b700efa9a99b589bc289edcf1
bmagalhaes-coder <bmagalhaes@acad.ifma.edu.br>
2026-03-14 15:31:49 -0300

    Update pesquisa.js

--- pesquisa.js
+++ pesquisa.js
@@ -20,8 +20,7 @@ const diasSemana = {
 
 const dadosSalas = {
 
-/* --- 1° ANDAR / TÉRREO --- */
-
+/* --- SALAS ADMINISTRATIVAS E APOIO --- */
 "SAA": {
     local: "Sala 55 - Bloco C - Térreo",
     horarios: {
@@ -49,10 +48,22 @@ const dadosSalas = {
     descricao: "Sala técnica de redes",
     observacoes: "Acesso restrito"
 },
+"NEOF/NLCC/SCDP/Contabilidade": {
+    local: "2° Andar",
+    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
+    descricao: "Setores financeiros e contábeis",
+    observacoes: "-"
+},
 "Arquivo": {
     local: "Bloco Administrativo",
     horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
     descricao: "Arquivo central",
+    observacoes: "-"
+},
+"NRH": {
+    local: "2° Andar",
+    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
+    descricao: "Núcleo de Recursos Humanos",
     observacoes: "-"
 },
 "Recpção/Espera": {
@@ -61,10 +72,22 @@ const dadosSalas = {
     descricao: "A preencher",
     observacoes: "-"
 },
+"DPG/NPGP": {
+    local: "2° Andar",
+    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
+    descricao: "A preencher",
+    observacoes: "-"
+},
 "Lavabo": {
     local: "Geral",
     horarios: { "Domingo": "Fechado", "Segunda": "Período Letivo", "Terça": "Período Letivo", "Quarta": "Período Letivo", "Quinta": "Período Letivo", "Sexta": "Período Letivo", "Sábado": "Fechado" },
     descricao: "Lavatórios",
+    observacoes: "-"
+},
+"DRG": {
+    local: "2° Andar",
+    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
+    descricao: "Diretoria Geral",
     observacoes: "-"
 },
 "Sala de Estudos Para Professores": {
@@ -85,6 +108,18 @@ const dadosSalas = {
     descricao: "A preencher",
     observacoes: "-"
 },
+"DDE": {
+    local: "2° Andar",
+    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
+    descricao: "Diretoria de Ensino",
+    observacoes: "-"
+},
+"DGP/NGP": {
+    local: "2° Andar",
+    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
+    descricao: "Gestão de Pessoas",
+    observacoes: "-"
+},
 "66 - AEE": {
     local: "Bloco C",
     horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
@@ -101,6 +136,12 @@ const dadosSalas = {
     local: "Térreo",
     horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
     descricao: "A preencher",
+    observacoes: "-"
+},
+"Diretoria": {
+    local: "2° Andar",
+    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
+    descricao: "Gabinete da Diretoria",
     observacoes: "-"
 },
 "Banheiro Feminino": {
@@ -128,7 +169,7 @@ const dadosSalas = {
     observacoes: "-"
 },
 
-/* --- SALAS DE AULA (TÉRREO) --- */
+/* --- SALAS DE AULA --- */
 "Sala 36": { local: "Bloco A", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
 "Sala 37": { local: "Bloco A", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
 "Sala 38": { local: "Bloco A", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
@@ -138,168 +179,6 @@ const dadosSalas = {
 "Sala 42": { local: "Bloco B", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
 "Sala 43": { local: "Bloco B", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
 "Sala 44": { local: "Bloco B", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
-"Sala 45": { local: "Bloco B", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
-"Sala 46": { local: "Bloco B", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
-"Sala 47": { local: "Bloco B", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
-"Sala 48": { local: "Bloco B", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
-"Sala 49": { local: "Bloco B", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
-"Sala 50": { local: "Bloco B", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
-"Sala 51": { local: "Bloco C", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
-"Sala 52": { local: "Bloco C", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
-
-"Sala 53": {
-    local: "Bloco C - Térreo",
-    descricao: "Sala de aula compartilhada por dois cursos técnicos e turmas do Proeja.",
-    observacoes: "Local de armazenamento de itens perdidos no campus. Qualquer objeto achado de titular desconhecido deve ser entregue neste departamento.",
-    cursos: [
-        { nome: "Edificações 2024", horarios: ["Segunda: 07:10 às 12:30", "Terça: 07:10 às 18:20", "Quarta: 07:10 às 12:30", "Quinta: 07:10 às 12:30", "Sexta: 07:10 às 12:30"] },
-        { nome: "Informática 2024", horarios: ["Segunda: 13:10 às 18:20", "Terça: 13:10 às 18:20", "Quarta: 13:10 às 18:20", "Quinta: 07:10 às 18:20", "Sexta: 13:10 às 18:20"] },
-        { nome: "Proeja 2025", horarios: ["Segunda: 18:20 às 22:50", "Terça: 18:20 às 22:50", "Quarta: 18:20 às 22:50", "Quinta: 18:20 às 22:50", "Sexta: 18:20 às 22:50"] } ],
-    equipamentos: "Televisão, quadro, ar condicionado"},
-
-"Sala 54": { local: "Bloco C", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
-"Sala 55": { local: "Bloco C", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
-"Sala 56": { local: "Bloco C", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
-"Sala 58": { local: "Bloco C", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
-"Sala 59": { local: "Bloco C", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
-"Sala 60": { local: "Bloco C", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
-
-"Psicologia": {
-    local: "Térreo",
-    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
-    descricao: "A preencher",
-    observacoes: "-"
-},
-"Setor Médico": {
-    local: "Térreo",
-    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
-    descricao: "A preencher",
-    observacoes: "-"
-},
-"Quadra": {
-    local: "Área Externa",
-    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
-    descricao: "A preencher",
-    observacoes: "-"
-},
-"Dispensa da Quadra": {
-    local: "Área Externa",
-    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
-    descricao: "A preencher",
-    observacoes: "-"
-},
-"Dispensa da Cozinha": {
-    local: "Anexo Cozinha",
-    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
-    descricao: "A preencher",
-    observacoes: "-"
-},
-
-/* --- 2° ANDAR --- */
-
-"NEOF/NLCC/SCDP/Contabilidade": {
-    local: "2° Andar",
-    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
-    descricao: "Setores financeiros e contábeis",
-    observacoes: "-"
-},
-"NRH": {
-    local: "2° Andar",
-    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
-    descricao: "Núcleo de Recursos Humanos",
-    observacoes: "-"
-},
-"DPG/NPGP": {
-    local: "2° Andar",
-    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
-    descricao: "A preencher",
-    observacoes: "-"
-},
-"DRG": {
-    local: "2° Andar",
-    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
-    descricao: "Diretoria Geral",
-    observacoes: "-"
-},
-"DDE": {
-    local: "2° Andar",
-    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
-    descricao: "Diretoria de Ensino",
-    observacoes: "-"
-},
-"DGP/NGP": {
-    local: "2° Andar",
-    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
-    descricao: "Gestão de Pessoas",
-    observacoes: "-"
-},
-"Diretoria": {
-    local: "2° Andar",
-    horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" },
-    descricao: "Gabinete da Diretoria",
-    observacoes: "-"
-}
-
-};
-
-function renderizarSalas() {
-    menuList.innerHTML = '';
-    [...salas].sort().forEach(sala => {
-        const btn = document.createElement('button');
-        btn.className = 'menu-item';
-        btn.textContent = sala;
-        btn.onclick = () => mostrarInfoSala(sala);
-        menuList.appendChild(btn);
-    });
-}
-
-function filtrar() {
-    const termo = normalizar(searchInput.value);
-    const botoes = document.querySelectorAll('.menu-item');
-    let encontrados = 0;
-
-    botoes.forEach(btn => {
-        const nomeSala = normalizar(btn.textContent);
-        if (nomeSala.includes(termo)) {
-            btn.classList.remove('hidden');
-            encontrados++;
-        } else {
-            btn.classList.add('hidden');
-        }
-    });
-
-    const erroExistente = document.querySelector('.sem-resultados');
-    if (encontrados === 0 && searchInput.value.length > 0) {
-        if (!erroExistente) {
-            const msg = document.createElement('div');
-            msg.className = 'sem-resultados';
-            msg.innerHTML = `Nenhum resultado para "<strong>${searchInput.value}</strong>"`;
-            menuList.appendChild(msg);
-        }
-    } else if (erroExistente) {
-        erroExistente.remove();
-    }
-}
-
-function getDiaAtual() {
-    const hoje = new Date();
-    const diaIngles = hoje.toLocaleDateString('en-US', { weekday: 'long' });
-    return diasSemana[diaIngles];
-}
-
-function isDiaAtual(horario) {
-    const diaAtual = getDiaAtual();
-    return horario.startsWith(diaAtual);
-}
-
-function mostrarInfoSala(nomeSala) {
-    const dados = dadosSalas[nomeSala] || {
-        local: "Informações não disponíveis",
-        descricao: "Detalhes desta sala serão atualizados em breve."
-    };
-    
-    const modal = document.createElement('div');
-    modalarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
 "Sala 45": { local: "Bloco B", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
 "Sala 46": { local: "Bloco B", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },
 "Sala 47": { local: "Bloco B", horarios: { "Domingo": "Fechado", "Segunda": "-", "Terça": "-", "Quarta": "-", "Quinta": "-", "Sexta": "-", "Sábado": "Fechado" }, descricao: "A preencher", observacoes: "-" },

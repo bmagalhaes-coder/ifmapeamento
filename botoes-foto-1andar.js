5c608ea19aead35338b399622a0d79b0d18df665
bmagalhaes-coder <bmagalhaes@acad.ifma.edu.br>
2026-05-18 12:03:14 -0300

    a

--- botoes-foto-1andar.js
+++ botoes-foto-1andar.js
@@ -17,21 +17,21 @@ const botoesGirado = [
 ];
 
 const botoesTerreo = [
-    { nome: "Banheiro F.\ne Acessível", x: 23.5, y: 91, w: 16, h: 4.5, fs: 2.33 },
-    { nome: "Banheiro M.\ne Acessível", x: 23.5, y: 9.3, w: 16, h: 4.5, fs: 2.33 },
-    { nome: "Copa", x: 42.3, y: 91, w: 16, h: 3, fs: 2.33 },
-    { nome: "Sala dos\nFuncionários\ne Servidores", x: 60.33, y: 91, w: 16, h: 6, fs: 2.33 },
-    { nome: "DAP", x: 72.5, y: 75, w: 16, h: 3, fs: 2.33 },
-    { nome: "Sala de\nVivência dos\nServidores", x: 45.6, y: 75, w: 16, h: 6, fs: 2.33 },
-    { nome: "Coordenação\nSuperior", x: 75.6, y: 60.6, w: 17, h: 4.5, fs: 2.33 },
-    { nome: "CGP", x: 75.6, y: 50.7, w: 16, h: 3, fs: 2.33 },
-    { nome: "CAPI/SCDP\nBalcão Digital", x: 75.6, y: 42, w: 17.5, h: 4.5, fs: 2.33 },
-    { nome: "CCTTII\nCEEC\nCCSAQ", x: 79.5, y: 32.5, w: 11, h: 6, fs: 2.33 },
-    { nome: "Arquivo\nCGP", x: 65, y: 33.9, w: 12, h: 4.5, fs: 2.33 },
-    { nome: "Coordenações", x: 73, y: 22, w: 18, h: 3, fs: 2.33 },
-    { nome: "CTIC", x: 69.5, y: 13, w: 16, h: 3, fs: 2.33 },
-    { nome: "Servidor\nCTIC", x: 77.4, y: 5.2, w: 12, h: 4.5, fs: 2.33 },
-    { nome: "DDE/DAP", x: 42.5, y: 9.3, w: 13, h: 3, fs: 2.33 },
+    { nome: "Banheiro F.\ne Acessível", x: 23.5, y: 91, w: 16, h: 4.5, fs: 2 },
+    { nome: "Banheiro M.\ne Acessível", x: 23.5, y: 9.3, w: 16, h: 4.5, fs: 2 },
+    { nome: "Copa", x: 42.3, y: 91, w: 16, h: 3, fs: 2 },
+    { nome: "Sala dos\nFuncionários\ne Servidores", x: 60.33, y: 91, w: 16, h: 6, fs: 2 },
+    { nome: "DAP", x: 72.5, y: 75, w: 16, h: 3, fs: 2 },
+    { nome: "Sala de\nVivência dos\nServidores", x: 45.6, y: 75, w: 16, h: 6, fs: 2 },
+    { nome: "Coordenação\nSuperior", x: 75.6, y: 60.6, w: 17, h: 4.5, fs: 2 },
+    { nome: "CGP", x: 75.6, y: 50.7, w: 16, h: 3, fs: 2 },
+    { nome: "CAPI/SCDP\nBalcão Digital", x: 75.6, y: 42, w: 17.5, h: 4.5, fs: 2 },
+    { nome: "CCTTII\nCEEC\nCCSAQ", x: 79.5, y: 32.5, w: 11, h: 6, fs: 2 },
+    { nome: "Arquivo\nCGP", x: 65, y: 33.9, w: 12, h: 4.5, fs: 2 },
+    { nome: "Coordenações", x: 73, y: 22, w: 18, h: 30, fs: 2 },
+    { nome: "CTIC", x: 69.5, y: 13, w: 16, h: 3, fs: 2 },
+    { nome: "Servidor\nCTIC", x: 77.4, y: 5.2, w: 12, h: 4.5, fs: 2 },
+    { nome: "DDE/DAP", x: 42.5, y: 9.3, w: 13, h: 3, fs: 2 },
 ];
 
 function renderBotoes(wrapper, img) {

26d6d13288bd8cd8e684baa89d3177acf14c1400
Maria Clara <bmagalhaes@acad.ifma.edu.br>
2026-06-18 14:14:54 -0300

    Update locais_terreo.js

--- locais_terreo.js
+++ locais_terreo.js
@@ -21,7 +21,7 @@ const salasTerreo = [
     { nome: "Banheiro Masculino e Acessível", x: 80.7, y: 62, w: 6.1, h: 3, cor: "clara" },
     { nome: "Banheiro Feminino e Acessível", x: 80.7, y: 67.5, w: 6.1, h: 3, cor: "clara" },
     { nome: "Sala58", x: 80.7, y: 70, w: 6.1, h: 4.2, cor: "clara" },
-    { nome: "Sala59", x: 80.7, y: 74, w: 6.1, h: 4.2, cor: "clara" },
+    { nome: "Sala59", x: 80.7, y: 74.5, w: 6.1, h: 4.2, cor: "clara" },
     { nome: "Sala60", x: 80.7, y: 79.45, w: 6.1, h: 4.2, cor: "clara" },
 
 

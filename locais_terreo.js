9d69127b37ce53d9d074cc1f85e57adbf74a99b7
Maria Clara <bmagalhaes@acad.ifma.edu.br>
2026-06-18 14:05:34 -0300

    Adjust coordinates for Sala59 and Sala60

--- locais_terreo.js
+++ locais_terreo.js
@@ -21,8 +21,8 @@ const salasTerreo = [
     { nome: "Banheiro Masculino e Acessível", x: 80.7, y: 62, w: 6.1, h: 3, cor: "clara" },
     { nome: "Banheiro Feminino e Acessível", x: 80.7, y: 67.5, w: 6.1, h: 3, cor: "clara" },
     { nome: "Sala58", x: 80.7, y: 70, w: 6.1, h: 4.2, cor: "clara" },
-    { nome: "Sala59", x: 80.7, y: 77, w: 6.1, h: 4.2, cor: "clara" },
-    { nome: "Sala60", x: 80.7, y: 79.1, w: 6.1, h: 4.2, cor: "clara" },
+    { nome: "Sala59", x: 80.7, y: 77.3, w: 6.1, h: 4.2, cor: "clara" },
+    { nome: "Sala60", x: 80.7, y: 79.2, w: 6.1, h: 4.2, cor: "clara" },
 
 
 

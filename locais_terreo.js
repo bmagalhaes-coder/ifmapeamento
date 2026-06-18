c7f018dcf9faf52d301c1200590df3b7e91e10b6
bmagalhaes-coder <bmagalhaes@acad.ifma.edu.br>
2026-06-18 15:35:59 -0300

    sd

--- locais_terreo.js
+++ locais_terreo.js
@@ -1,33 +1,52 @@
 const salasTerreo = [
 //quadra
-    { nome: "corredor11", x: 75.3, y: 21, w: 2.2, h: 44, cor: "escura" },
-    { nome: "corredor12", x: 26.5, y: 3, w: 28, h: 15, cor: "escura" },
-    { nome: "corredor13", x: 54, y: 4.9, w: 10, h: 12, cor: "escura" },
-    { nome: "corredor14", x: 64, y: 0.1, w: 20.9, h: 21, cor: "escura" },
+    { nome: "corredor 11", x: 75.3, y: 21, w: 2.2, h: 44, cor: "escura" },
+    { nome: "corredor 12", x: 26.5, y: 3, w: 28, h: 15, cor: "escura" },
+    { nome: "corredor 13", x: 54, y: 4.9, w: 10, h: 12, cor: "escura" },
+    { nome: "corredor 14", x: 64, y: 0.1, w: 20.9, h: 21, cor: "escura" },
     { nome: "piscina", x: 30.4, y: 6, w: 20, h: 9, cor: "clara" },
     { nome: "dispensa da quadra", x: 54.5, y: 5.8, w: 9.3, h: 3.5, cor: "clara" },
     { nome: "banheiro feminino com chuveiro", x: 54.5, y: 9.05, w: 9.3, h: 3.5, cor: "clara" },
     { nome: "banheiro masculino com chuveiro", x: 54.5, y: 12.2, w: 9.3, h: 3.5, cor: "clara" },
     { nome: "quadra", x: 66.8, y: 0.1, w: 18, h: 18.7, cor: "clara" },
 //bloco D
-    { nome: "corredor10", x: 91.3, y: 51.6, w: 8, h: 30.1, cor: "escura" },
-    { nome: "blocoD", x: 93.2, y: 51.6, w: 6.1, h: 30.1, cor: "clara" },
+    { nome: "corredor 10", x: 91.5, y: 51.6, w: 8, h: 30.1, cor: "escura" },
+    { nome: "blocoD", x: 93.4, y: 51.6, w: 6.1, h: 30.1, cor: "clara" },
 
 //bloco C
-    { nome: "corredor9", x: 78.8, y: 49.66, w: 8, h: 33.9, cor: "escura" },
-    { nome: "lab. de Produção Mecânica", x: 80.7, y: 49.66, w: 6.1, h: 4.2, cor: "clara" },
-    { nome: "SAA", x: 80.7, y: 53.8, w: 6.1, h: 4.2, cor: "clara" },
-    { nome: "Lab. de Soldagem", x: 80.7, y: 57.9, w: 6.1, h: 4.2, cor: "clara" },
-    { nome: "Banheiro Masculino e Acessível", x: 80.7, y: 62, w: 6.1, h: 3.5, cor: "clara" },
-    { nome: "Banheiro Feminino e Acessível", x: 80.7, y: 67.5, w: 6.1, h: 3.5, cor: "clara" },
-    { nome: "Sala58", x: 80.7, y: 70.7, w: 6.1, h: 4.3, cor: "clara" },
-    { nome: "Sala59", x: 80.7, y: 74.8, w: 6.1, h: 4.3, cor: "clara" },
-    { nome: "Sala60", x: 80.7, y: 79.4, w: 6.1, h: 4.3, cor: "clara" },
-
-
-
+    { nome: "corredor 9", x: 78.8, y: 49.64, w: 8, h: 33.35, cor: "escura" },
+    { nome: "lab. de Produção Mecânica", x: 80.7, y: 49.6, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "SAA", x: 80.7, y: 53.6, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Lab. de Soldagem", x: 80.7, y: 57.7, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Banheiro Masculino e Acessível", x: 80.7, y: 61.8, w: 6.1, h: 3.2, cor: "clara" },
+    { nome: "Banheiro Feminino e Acessível", x: 80.7, y: 67.4, w: 6.1, h: 3.2, cor: "clara" },
+    { nome: "Sala 58", x: 80.7, y: 70.5, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Sala 59", x: 80.7, y: 74.6, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Sala 60", x: 80.7, y: 78.8, w: 6.1, h: 4.3, cor: "clara" },
 //bloco B
-
+    { nome: "corredor 8", x: 64.5, y: 45.32, w: 8, h: 41.85, cor: "escura" },
+    { nome: "Sala 46", x: 64.5, y: 45.32, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Sala 47", x: 64.5, y: 49.5, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Sala 48", x: 64.5, y: 53.7, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Sala 49", x: 64.5, y: 57.8, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Banheiro Masculino e Acessível", x: 64.5, y: 61.8, w: 6.1, h: 3.2, cor: "clara" },
+    { nome: "Banheiro Feminino e Acessível", x: 64.5, y: 67.4, w: 6.1, h: 3.2, cor: "clara" },
+    { nome: "Sala 46", x: 64.5, y: 70.5, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Sala 47", x: 64.5, y: 74.6, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Sala 48", x: 64.5, y: 78.8, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Sala 49", x: 64.5, y: 83, w: 6.1, h: 4.3, cor: "clara" },
+//bloco A
+    { nome: "corredor 7", x: 49, y: 43.89, w: 8, h: 44.5, cor: "escura" },
+    { nome: "Lab. de Máquinas Elétricas", x: 49, y: 43.89, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Lab. de Medidas Elétricas", x: 49, y: 48.07, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Lab. de Física e Eletrônica", x: 49, y: 52.25, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Lab. de Informática (superior)", x: 49, y: 56.43, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Fábrica de Inovações", x: 49, y: 60.61, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Lab. de química", x: 49, y: 67.4, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Lab. de biologia", x: 49, y: 71.58, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Lab. de Desenho Técnico", x: 49, y: 75.76, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Lab. de Informática", x: 49, y: 79.94, w: 6.1, h: 4.3, cor: "clara" },
+    { nome: "Lab. de Informática", x: 49, y: 84.12, w: 6.1, h: 4.3, cor: "clara" },
 ];
 
 function desenharSalasTerreo() {
